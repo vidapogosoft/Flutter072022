2ab2d28062125cef1b8a13a65f4512cbb2765702
vidapogosoft <vidapogosoft@hotmail.com>
2022-08-05 20:20:17 -0500

    correccion de identity

--- dia4/clases/sqlite_ui/lib/db/db.dart
+++ dia4/clases/sqlite_ui/lib/db/db.dart
@@ -49,6 +49,9 @@ class Db {
   //Insert
   static addClientToDatabase(Client client) async {
     Database database = await openDB();
+    var table = await database.rawQuery("SELECT MAX(id)+1 as id FROM Client");
+    int? id = table.first["id"] as int?;
+    client.id = id!;
     var raw = await database.insert("Client", client.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
     return raw;
